79cae735e9e38d87f7e9dffaffd48338d7402419
proSamik <samikchoudhury15@gmail.com>
2025-02-23 21:40:38 +0530

    refactor: Remove Navigation component from Contact page

--- client/src/app/contact/page.tsx
+++ client/src/app/contact/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState } from 'react'
-import { Navigation } from '@/components/Navigation'
 import { Footer } from '@/components/Footer'
 
 /**
@@ -23,7 +22,6 @@ export default function Contact() {
 
   return (
     <div className="min-h-screen bg-light-background dark:bg-dark-background">
-      <Navigation />
 
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
         <div className="mx-auto max-w-3xl text-center">
